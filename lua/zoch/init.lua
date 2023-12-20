152bb5a0297c01d087fd53ac6cc2f9e3fd064bac
Zoclhas <zoclhas@gmail.com>
2023-12-20 17:22:19 +0400

    stuff

--- lua/zoch/init.lua
+++ lua/zoch/init.lua
@@ -1,1 +1,1 @@
-
+require("zoch.remap")
---
+++ lua/zoch/packer.lua
@@ -0,0 +1,24 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+	
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use({
+	  'kabouzeid/nvim-jellybeans',
+	  as = 'jellybeans',
+	  requires = "rktjmp/lush.nvim",
+	  config = function()
+		  vim.cmd('colorscheme jellybeans')
+	  end
+  })
+end)
