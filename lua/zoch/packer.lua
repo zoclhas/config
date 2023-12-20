1eb030920f33bcfca5a1656ca367525bc0107509
Zoclhas <zoclhas@gmail.com>
2023-12-20 18:41:01 +0400

    add undotree and fugitive

--- lua/zoch/packer.lua
+++ lua/zoch/packer.lua
@@ -24,4 +24,7 @@ return require('packer').startup(function(use)
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use("ThePrimeagen/harpoon")
+  use("mbbill/undotree")
+  use("tpope/vim-fugitive")
+
 end)
