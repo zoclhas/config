82b1302f4e46a9249a71731543af60febcf608f3
Zoclhas <zoclhas@gmail.com>
2023-12-20 21:16:18 +0400

    catpuccin theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,27 @@
 return {
   -- Jellybeans theme
-  { "rktjmp/lush.nvim" },
-  { "metalelf0/jellybeans-nvim" },
+  -- { "rktjmp/lush.nvim" },
+  -- { "metalelf0/jellybeans-nvim" },
+
+  -- Catpuccin
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = true,
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background = true,
+      })
+    end,
+  },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "jellybeans-nvim",
+      --  colorscheme = "jellybeans-nvim",
+      colorscheme = "catppuccin",
     },
   },
 }
