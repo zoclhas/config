7cfb792c9a7f199b69ccd6c7b503f589e9983932
zoclhas <zoclhas@gmail.com>
2024-02-03 20:45:11 +0400

    Added tailwind sorter

--- lua/plugins/typescript.lua
+++ lua/plugins/typescript.lua
@@ -128,4 +128,16 @@ return {
       end
     end,
   },
+
+  {
+    "laytan/tailwind-sorter.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
+    build = "cd formatter && npm i && npm run build",
+    config = function()
+      require("tailwind-sorter").setup({
+        on_save_enabled = true,
+        on_save_pattern = { "*.html", "*.jsx", "*.tsx", "*.rs" },
+      })
+    end,
+  },
 }
