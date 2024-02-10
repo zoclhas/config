a71625c8954e392a19fc51da8a1d9d07950699cd
zoclhas <zoclhas@gmail.com>
2024-02-10 11:34:22 +0400

    Neovide stuff

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -18,3 +18,15 @@ vim.opt.incsearch = true
 opt.scrolloff = 8
 
 opt.wrap = true
+
+if vim.g.neovide then
+  vim.o.guifont = "JetBrainsMono Nerd Font:h11"
+
+  -- MacOS only :(((
+  vim.g.neovide_background_color = "#0f1117"
+
+  vim.g.neovide_refresh_rate = 144
+  vim.g.neovide_refresh_rate_idle = 5
+
+  vim.g.neovide_cursor_animate_command_line = false
+end
